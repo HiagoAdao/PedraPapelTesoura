a7c87ac442a1b3535b7c671b042fdeb5013b5131
Hiago Adao <hiago@registro.rs>
2021-11-22 15:26:00 -0300

    feat: Implementando classe para timer.

--- pedra_papel_tesoura/util.py
+++ pedra_papel_tesoura/util.py
@@ -1,5 +1,5 @@
-from typing import Any, Union
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import Any, Union, Callable
 
 
 class Util:
@@ -15,3 +15,31 @@ class Util:
         current_date = datetime.today()
         formated_date = current_date.strftime(format_date)
         return formated_date
+
+
+class Timer:
+    def __init__(self):
+        self.__start_time: float = None
+        self.__end_time: Union[int, float] = None
+
+    @property
+    def is_running(self) -> bool:
+        return bool(self.__start_time and self.__end_time)
+
+    def start(self, time: Union[int, float]) -> None:
+        current_time = self.__get_current_date()
+        self.__start_time: datetime = current_time
+        self.__end_time = current_time + timedelta(0, time)
+
+    def execute_function_when_end(self, func: Callable) -> None:
+        if not self.__start_time or not self.__end_time:
+            return
+
+        current_time = self.__get_current_date()
+        print(current_time, self.__end_time, current_time >= self.__end_time)
+        if current_time >= self.__end_time:
+            self.__start_time = None
+            func()
+
+    def __get_current_date(self) -> datetime:
+        return datetime.today()
